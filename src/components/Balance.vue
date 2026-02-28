1200b0a0f8500e5259d105b4883f59894bbe9474
edvillatoro2 <edvillatoro2@gmail.com>
2026-02-27 20:22:31 -0500

    adds balance and expenses components

---
+++ src/components/Balance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="balance">
+    <h2>Balance</h2>
+    <h1 class="balance">{{ total }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.balance {
+  color: #42b883;
+}
+</style>
---
+++ src/components/Expenses.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="expenses">
+    <h2>Expenses</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+    income: {
+        type: Number,
+        required: true,
+    },
+    expenses: {
+        type: Number,
+        required: true,
+    }
+})
+</script>
+
+<style scoped>
+.expenses {
+  color: #ff5252;
+}
+</style>
