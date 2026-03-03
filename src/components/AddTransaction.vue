a4a4107060c324b9be79235ab7cc36cbef52b070
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-03 13:35:14 -0500

    binds refs to amount and text inputs

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -7,7 +7,7 @@
         <input
           type="text"
           id="text"
-          ref="text"
+          v-model="text"
           class="w-full px-3 py-2 text-gray-700 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 hover:border-gray-400"
           placeholder="enter title..."
         />
@@ -16,7 +16,7 @@
         <label for="amount">amount</label>
         <input
           type="number"
-          ref="amount"
+          v-model="amount"
           id="amount"
           class="w-full px-3 py-2 text-gray-700 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 hover:border-gray-400"
           placeholder="enter amount..."
